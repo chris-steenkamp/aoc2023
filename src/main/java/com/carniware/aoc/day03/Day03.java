5cd8906f2ce38d155d178d2dfa7a55abb0d4ae76
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-03 11:21:44 +0000

    Refactor so both parts use the functions.

--- src/main/java/com/carniware/aoc/day03/Day03.java
+++ src/main/java/com/carniware/aoc/day03/Day03.java
@@ -21,56 +21,34 @@ import com.carniware.aoc.common.Helper;
 public class Day03 implements AoCDay {
     private List<String> input;
     private List<Set<Integer>> symbolPositions;
-    private List<Map<Integer, List<Integer>>> potentialGears;
+    private List<Map<Integer, Set<Integer>>> potentialGears;
     private int part1Result;
     private int part2Result;
 
     public Day03() {
-        this("src/main/java/com/carniware/aoc/day03/sample.txt");
+        this("src/main/java/com/carniware/aoc/day03/input.txt");
     }
 
     public Day03(String filename) {
         input = Helper.readFile(filename);
         findSymbols();
-    }
-
-    public Day03(String[] inputs) {
-        input = List.of(inputs);
-        findSymbols();
+        calculate();
     }
 
     public int getPart1Result() {
-        runPart1();
         return part1Result;
     }
 
     public int getPart2Result() {
-        runPart2();
         return part2Result;
     }
 
     public void runPart1() {
-        int height = input.size();
-
-        Pattern digits = Pattern.compile("\\d+");
-        Matcher m = digits.matcher("");
-
-        ArrayList<Integer> validPartNumbers = new ArrayList<>();
-
-        for (int y_ = 0; y_ < height; ++y_) {
-            final int y = y_;
-            m.reset(input.get(y));
-
-            while (m.find()) {
-                if (IntStream.range(m.start(), m.end()).anyMatch(x -> findAdjacentSymbols(x, y))) {
-                    validPartNumbers.add(Integer.parseInt(m.group()));
-                }
-            }
-        }
-
-        part1Result = validPartNumbers.stream().reduce(0, (t, u) -> t + u);
-
         System.out.println(part1Result);
+    }
+
+    public void runPart2() {
+        System.out.println(part2Result);
     }
 
     private void findSymbols() {
@@ -82,30 +60,16 @@ public class Day03 implements AoCDay {
         for (var line : input) {
             m.reset(line);
             Set<Integer> symbolPosition = new HashSet<>();
-            Map<Integer, List<Integer>> potentialGearPosition = new HashMap<>();
+            Map<Integer, Set<Integer>> potentialGearPosition = new HashMap<>();
             while (m.find()) {
                 symbolPosition.add(m.start());
                 if ("*".equals(m.group())) {
-                    potentialGearPosition.put(m.start(), new ArrayList<>());
+                    potentialGearPosition.put(m.start(), new HashSet<>());
                 }
             }
             symbolPositions.add(symbolPosition);
             potentialGears.add(potentialGearPosition);
         }
-    }
-
-    private Boolean findAdjacentSymbols(int x, int y) {
-        int start = y == 0 ? 0 : y - 1;
-        int end = y == input.size() - 1 ? input.size() - 1 : y + 1;
-
-        for (int i = start; i <= end; ++i) {
-            if (symbolPositions.get(i).contains(x) || symbolPositions.get(i).contains(x - 1)
-                    || symbolPositions.get(i).contains(x + 1)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     private Boolean findAdjacentSymbols(int x, int y, int partNumber) {
@@ -126,7 +90,7 @@ public class Day03 implements AoCDay {
         return false;
     }
 
-    public void runPart2() {
+    private void calculate() {
         int height = input.size();
 
         Pattern digits = Pattern.compile("\\d+");
@@ -147,13 +111,14 @@ public class Day03 implements AoCDay {
             }
         }
 
+        part1Result = validPartNumbers.stream()
+                .reduce(0, (t, u) -> t + u);
+
         part2Result = potentialGears.stream()
                 .filter(k -> k.values().size() > 0)
                 .flatMap(t -> t.values().stream())
                 .filter(l -> l.size() == 2)
                 .map(l -> l.stream().reduce(1, (t, u) -> t * u))
                 .reduce(0, (t, u) -> t + u);
-
-        System.out.println(part2Result);
     }
 }
