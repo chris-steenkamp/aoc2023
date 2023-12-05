93287137e7d23b9b72464e534f2cb1a2820fbc62
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-05 12:07:32 +0000

    Update template to extend abstract class.

--- src/main/java/com/carniware/aoc/template/DayN.java
+++ src/main/java/com/carniware/aoc/template/DayN.java
@@ -1,36 +1,23 @@
 package com.carniware.aoc.template;
 
-import java.util.List;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import com.carniware.aoc.common.AoCDay;
-import com.carniware.aoc.common.Helper;
+import com.carniware.aoc.common.AoCDayAbstract;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class DayN implements AoCDay {
-    private List<String> input;
-
+public class DayN extends AoCDayAbstract {
     public DayN() {
         this("src/main/java/com/carniware/aoc/dayn/input.txt");
     }
 
     public DayN(String filename) {
-        input = Helper.readFile(filename);
+        super(filename);
     }
 
     public DayN(String[] inputs) {
-        input = List.of(inputs);
-    }
-
-    public void runPart1() {
-        System.out.println("Part1 output");
-    }
-
-    public void runPart2() {
-        System.out.println("Part2 output");
+        super(inputs);
     }
 }
