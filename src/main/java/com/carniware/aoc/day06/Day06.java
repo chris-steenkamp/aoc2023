2e32d5217e1ce33452bbd49c305db16a03ab7b85
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-06 13:49:36 +0000

    Add day06 part 1.

---
+++ src/main/java/com/carniware/aoc/day06/Day06.java
@@ -0,0 +1,62 @@
+package com.carniware.aoc.day06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.carniware.aoc.common.AoCDayAbstract;
+
+@Component
+@Order(6)
+public class Day06 extends AoCDayAbstract {
+    public Day06() {
+        this("src/main/java/com/carniware/aoc/day06/sample.txt");
+    }
+
+    public Day06(String filename) {
+        super(filename);
+        calculate();
+    }
+
+    private void calculate() {
+        var times = Arrays.stream(
+                input.get(0)
+                        .replace("Time:", "")
+                        .trim()
+                        .split("\s+"))
+                .filter(x -> !x.isEmpty())
+                .map(x -> Integer.parseInt(x))
+                .toList();
+
+        var distances = Arrays.stream(
+                input.get(1)
+                        .replace("Distance:", "")
+                        .trim()
+                        .split("\s+"))
+                .filter(x -> !x.isEmpty())
+                .map(x -> Integer.parseInt(x))
+                .toList();
+
+        List<Integer> winners = new ArrayList<>(Collections.nCopies(times.size(), 0));
+
+        IntStream.range(0, times.size()).parallel().forEach(x -> {
+            var raceTime = times.get(x);
+            for (var i = 0; i < raceTime; ++i) {
+                var distanceTravelled = (raceTime - i) * i;
+
+
+                if (distanceTravelled > distances.get(x)) {
+                    winners.set(x, winners.get(x)+1);
+                }
+                
+            }
+        });
+
+        part1Result = winners.stream().reduce(1, (t, u) -> t * u);
+    }
+}
