245664e3b0c97e4a21e6500859cd13f617a70f0e
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-06 14:00:59 +0000

    Update main program to load beans matching interface.

--- src/main/java/com/carniware/aoc/AocApplication.java
+++ src/main/java/com/carniware/aoc/AocApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.carniware.aoc.common.AoCDayAbstract;
+import com.carniware.aoc.common.AoCDay;
 
 @SpringBootApplication
 public class AocApplication implements CommandLineRunner {
@@ -16,7 +16,7 @@ public class AocApplication implements CommandLineRunner {
 	}
 
     @Autowired
-    private List<AoCDayAbstract> daysImplemented;
+    private List<AoCDay> daysImplemented;
 
     @Override
     public void run(String... args) throws Exception {
