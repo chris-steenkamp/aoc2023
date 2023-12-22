bb6447603cd2f3c2b7f2337688ff3c8a3fc5bdf5
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-22 14:15:13 +0000

    Day 20 part 1.

---
+++ src/main/java/com/carniware/aoc/day20/Day20.java
@@ -0,0 +1,101 @@
+package com.carniware.aoc.day20;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.carniware.aoc.common.AoCDayAbstract;
+
+@Component
+@Order(20)
+public class Day20 extends AoCDayAbstract {
+    public Day20() {
+        this("src/main/java/com/carniware/aoc/day20/input.txt");
+    }
+
+    public Day20(String filename) {
+        super(filename);
+        calculate();
+    }
+
+    public Day20(String[] inputs) {
+        super(inputs);
+        calculate();
+    }
+
+    private void calculate() {
+        Map<String, Set<String>> moduleConnections = new LinkedHashMap<>();
+        Map<String, Module> modules = new HashMap<>();
+        Map<String, Set<String>> moduleInputs = new HashMap<>();
+
+        var button = new ButtonModule();
+        modules.put(button.name, button);
+        moduleConnections.put(button.name, Set.of("broadcaster"));
+
+        for (var line : input) {
+            var parts = line.split("( -> )");
+
+            var module = Module.fromString(parts[0]);
+            var connections = new HashSet<>(Arrays.stream(parts[1].split(",")).map(x -> x.trim()).toList());
+            moduleConnections.put(module.name, connections);
+            connections.stream().forEach(x -> {
+                var currentInputs = moduleInputs.getOrDefault(x, new HashSet<>());
+                currentInputs.add(module.name);
+                moduleInputs.put(x, currentInputs);
+                // if a module doesn't yet exist, add it as an output module.
+                // If it exists on the LHS of a an input string then it will be
+                // redefined as the correct type (flipflop, conjunction, broadcaster)
+                modules.putIfAbsent(x, Module.fromString(x));
+                moduleConnections.putIfAbsent(x, Set.of());
+            });
+            modules.put(module.name, module);
+        }
+
+        // Recreate each conjunction module with the correct list of input modules.
+        // This will initialize the module's input memory to LOW for all inputs.
+        for (var module : modules.keySet()) {
+            var m = modules.get(module);
+            if (m instanceof ConjunctionModule) {
+                modules.put(m.name, new ConjunctionModule(m.name, moduleInputs.get(m.name)));
+            }
+        }
+
+        calculatePart1(modules, moduleConnections);
+    }
+
+    private void calculatePart1(Map<String, Module> modules, Map<String, Set<String>> moduleConnections) {
+        Queue<Pulse> queue = new LinkedList<>();
+
+        long totalHigh = 0;
+        long totalLow = 0;
+        for (var i = 0; i < 1000; ++i) {
+            // System.out.println("--------------------------");
+            queue.add(new Pulse(modules.get("button"), modules.get("broadcaster"), 0, PulseState.LOW));
+            while (!queue.isEmpty()) {
+                var pulse = queue.poll();
+                var result = pulse.receiver().handlePulse(pulse);
+                if (pulse.state().equals(PulseState.HIGH)) {
+                    ++totalHigh;
+                } else {
+                    ++totalLow;
+                }
+
+                if (result != null) {
+                    for (var connection : moduleConnections.get(pulse.receiver().name)) {
+                        queue.add(new Pulse(pulse.receiver(), modules.get(connection), pulse.id() + 1, result));
+                    }
+                }
+            }
+        }
+
+        part1Result = totalHigh * totalLow;
+    }
+}
---
+++ src/main/java/com/carniware/aoc/day20/Module.java
@@ -0,0 +1,122 @@
+package com.carniware.aoc.day20;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+enum PulseState {
+    LOW,
+    HIGH
+}
+
+record Pulse(Module sender, Module receiver, long id, PulseState state) {
+}
+
+public abstract class Module {
+    String name;
+
+    public Module(String name) {
+        this.name = name;
+    }
+
+    public static Module fromString(String input) {
+        var parts = input.splitWithDelimiters("&|%", 0);
+        if (parts.length == 1) {
+            switch (parts[0]) {
+                case "broadcaster":
+                    return new BroadcastModule();
+                default:
+                    return new OutputModule(parts[0]);
+            }
+        } else {
+            switch (parts[1].charAt(0)) {
+                case '%':
+                    return new FlipFlopModule(parts[2]);
+                case '&':
+                    return new ConjunctionModule(parts[2]);
+            }
+        }
+        return null;
+    }
+
+    public PulseState handlePulse(Pulse pulse) {
+        System.out.println(
+                String.format("%s -%s-> %s", pulse.sender(), pulse.state().toString().toLowerCase(), pulse.receiver()));
+        return pulse.state();
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
+
+class ButtonModule extends Module {
+    public ButtonModule() {
+        super("button");
+    }
+}
+
+class BroadcastModule extends Module {
+    public BroadcastModule() {
+        super("broadcaster");
+    }
+}
+
+class OutputModule extends Module {
+    public OutputModule(String name) {
+        super(name);
+    }
+}
+
+class FlipFlopModule extends Module {
+    private Boolean on;
+
+    public FlipFlopModule(String name) {
+        super(name);
+        this.on = false;
+    }
+
+    @Override
+    public PulseState handlePulse(Pulse pulse) {
+        super.handlePulse(pulse);
+        PulseState returnPulse = null;
+        if (pulse.state().equals(PulseState.LOW)) {
+            // if the module was on, turn off and send a low pulse
+            if (this.on) {
+                returnPulse = PulseState.LOW;
+            } else {
+                returnPulse = PulseState.HIGH;
+            }
+            this.on = !this.on;
+        }
+        // System.out.println(String.format("%s state changed to %s", this, this.on ?
+        // "on" : "off"));
+        return returnPulse;
+    }
+}
+
+class ConjunctionModule extends Module {
+    private Map<String, PulseState> inputModules;
+
+    public ConjunctionModule(String name) {
+        this(name, Set.of());
+    }
+
+    public ConjunctionModule(String name, Set<String> inputModules) {
+        super(name);
+        this.inputModules = new HashMap<>();
+        inputModules.forEach(x -> this.inputModules.put(x, PulseState.LOW));
+    }
+
+    @Override
+    public PulseState handlePulse(Pulse pulse) {
+        super.handlePulse(pulse);
+        inputModules.put(pulse.sender().name, pulse.state());
+        boolean allHigh = inputModules.values().stream().allMatch(x -> x.equals(PulseState.HIGH));
+        // System.out.println(String.format("%s %s all remembered inputs are high",
+        // this, allHigh ? "" : "not "));
+        return allHigh ? PulseState.LOW : PulseState.HIGH;
+    }
+
+}
