b1ce233fc6cb0b99461943762ccb944a0a116d67
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-12-23 08:22:03 +0000

    Day 20 part 2.

--- src/main/java/com/carniware/aoc/day20/Day20.java
+++ src/main/java/com/carniware/aoc/day20/Day20.java
@@ -33,20 +33,31 @@ public class Day20 extends AoCDayAbstract {
     }
 
     private void calculate() {
-        Map<String, Set<String>> moduleConnections = new LinkedHashMap<>();
+        calculatePart1();
+        calculatePart2();
+    }
+
+    record FunctionInputs(
+            Map<String, Module> modules,
+            Map<String, Set<String>> moduleOutputs,
+            Map<String, Set<String>> moduleInputs) {
+    }
+
+    private FunctionInputs buildModules() {
+        Map<String, Set<String>> moduleOutputs = new LinkedHashMap<>();
         Map<String, Module> modules = new HashMap<>();
         Map<String, Set<String>> moduleInputs = new HashMap<>();
 
         var button = new ButtonModule();
         modules.put(button.name, button);
-        moduleConnections.put(button.name, Set.of("broadcaster"));
+        moduleOutputs.put(button.name, Set.of("broadcaster"));
 
         for (var line : input) {
             var parts = line.split("( -> )");
 
             var module = Module.fromString(parts[0]);
             var connections = new HashSet<>(Arrays.stream(parts[1].split(",")).map(x -> x.trim()).toList());
-            moduleConnections.put(module.name, connections);
+            moduleOutputs.put(module.name, connections);
             connections.stream().forEach(x -> {
                 var currentInputs = moduleInputs.getOrDefault(x, new HashSet<>());
                 currentInputs.add(module.name);
@@ -55,7 +66,7 @@ public class Day20 extends AoCDayAbstract {
                 // If it exists on the LHS of a an input string then it will be
                 // redefined as the correct type (flipflop, conjunction, broadcaster)
                 modules.putIfAbsent(x, Module.fromString(x));
-                moduleConnections.putIfAbsent(x, Set.of());
+                moduleOutputs.putIfAbsent(x, Set.of());
             });
             modules.put(module.name, module);
         }
@@ -69,17 +80,18 @@ public class Day20 extends AoCDayAbstract {
             }
         }
 
-        // calculatePart1(modules, moduleConnections);
-        calculatePart2(modules, moduleConnections, moduleInputs.get("nr"));
+        return new FunctionInputs(modules, moduleOutputs, moduleInputs);
     }
 
-    private void calculatePart1(Map<String, Module> modules, Map<String, Set<String>> moduleConnections) {
+    private void calculatePart1() {
+        var inputs = buildModules();
+        var modules = inputs.modules;
+        var moduleOutputs = inputs.moduleOutputs;
         Queue<Pulse> queue = new LinkedList<>();
 
         long totalHigh = 0;
         long totalLow = 0;
         for (var i = 0; i < 1000; ++i) {
-            // System.out.println("--------------------------");
             queue.add(new Pulse(modules.get("button"), modules.get("broadcaster"), 0, PulseState.LOW));
             while (!queue.isEmpty()) {
                 var pulse = queue.poll();
@@ -91,7 +103,7 @@ public class Day20 extends AoCDayAbstract {
                 }
 
                 if (result != null) {
-                    for (var connection : moduleConnections.get(pulse.receiver().name)) {
+                    for (var connection : moduleOutputs.get(pulse.receiver().name)) {
                         queue.add(new Pulse(pulse.receiver(), modules.get(connection), pulse.id() + 1, result));
                     }
                 }
@@ -101,37 +113,47 @@ public class Day20 extends AoCDayAbstract {
         part1Result = totalHigh * totalLow;
     }
 
-    private void calculatePart2(Map<String, Module> modules, Map<String, Set<String>> moduleConnections,
-            Set<String> modulesToWatch) {
+    private void calculatePart2() {
+        var inputs = buildModules();
+        var modules = inputs.modules;
+        var moduleConnections = inputs.moduleOutputs;
+        var moduleInputs = inputs.moduleInputs;
+        var finalInverter = moduleInputs.keySet()
+                .stream()
+                .filter(x -> modules.get(x) instanceof ConjunctionModule)
+                .map(x -> ((ConjunctionModule) modules.get(x)))
+                .filter(x -> x.getInputModules().size() == 4)
+                .findFirst();
+
+        var modulesToWatch = finalInverter.isPresent() ? finalInverter.get().getInputModules().keySet() : Set.of();
+
         Queue<Pulse> queue = new LinkedList<>();
         Map<String, Long> cycles = new HashMap<>();
 
         long i = 0;
-        if (modules.containsKey("rx")) {
-            while (true) {
-                ++i;
-                queue.add(new Pulse(modules.get("button"), modules.get("broadcaster"), 0, PulseState.LOW));
-                while (!queue.isEmpty()) {
-                    var pulse = queue.poll();
-                    var result = pulse.receiver().handlePulse(pulse);
+        while (true) {
+            ++i;
+            queue.add(new Pulse(modules.get("button"), modules.get("broadcaster"), 0, PulseState.LOW));
+            while (!queue.isEmpty()) {
+                var pulse = queue.poll();
+                var result = pulse.receiver().handlePulse(pulse);
 
-                    if (result != null) {
-                        for (var connection : moduleConnections.get(pulse.receiver().name)) {
-                            queue.add(new Pulse(pulse.receiver(), modules.get(connection), pulse.id() + 1, result));
-                        }
+                if (result != null) {
+                    for (var connection : moduleConnections.get(pulse.receiver().name)) {
+                        queue.add(new Pulse(pulse.receiver(), modules.get(connection), pulse.id() + 1, result));
                     }
-                    if (modulesToWatch.contains(pulse.sender().name)) {
-                        if (pulse.state().equals(PulseState.HIGH)) {
-                            if (!cycles.containsKey(pulse.sender().name)) {
-                                System.out.println(String.format("Found minimum for %s (%d)", pulse.sender().name, i));
-                                cycles.put(pulse.sender().name, i);
-                            }
+                }
+                if (modulesToWatch.contains(pulse.sender().name)) {
+                    if (pulse.state().equals(PulseState.HIGH)) {
+                        if (!cycles.containsKey(pulse.sender().name)) {
+                            System.out.println(String.format("Found minimum for %s (%d)", pulse.sender().name, i));
+                            cycles.put(pulse.sender().name, i);
                         }
                     }
                 }
-                if (cycles.size() == modulesToWatch.size()) {
-                    break;
-                }
+            }
+            if (cycles.size() == modulesToWatch.size()) {
+                break;
             }
         }
         part2Result = cycles.isEmpty() ? 0 : cycles.values().stream().reduce(Helper::lcm).get();
--- src/test/java/com/carniware/aoc/Day20Test.java
+++ src/test/java/com/carniware/aoc/Day20Test.java
@@ -10,9 +10,9 @@ import com.carniware.aoc.day20.Day20;
 class Day20Test {
     @Test
     void test() {
-        // assertEquals(32000000, new Day20("src/main/java/com/carniware/aoc/day20/sample.txt").getPart1Result());
-        // assertEquals(11687500, new Day20("src/main/java/com/carniware/aoc/day20/sample2.txt").getPart1Result());
-        // assertEquals(814934624, new Day20("src/main/java/com/carniware/aoc/day20/input.txt").getPart1Result());
+        assertEquals(32000000, new Day20("src/main/java/com/carniware/aoc/day20/sample.txt").getPart1Result());
+        assertEquals(11687500, new Day20("src/main/java/com/carniware/aoc/day20/sample2.txt").getPart1Result());
+        assertEquals(814934624, new Day20("src/main/java/com/carniware/aoc/day20/input.txt").getPart1Result());
         assertTrue(19296496614487950l > new Day20("src/main/java/com/carniware/aoc/day20/input.txt").getPart2Result());
         assertTrue(69406901511717l < new Day20("src/main/java/com/carniware/aoc/day20/input.txt").getPart2Result());
         assertEquals(228282646835717l, new Day20("src/main/java/com/carniware/aoc/day20/input.txt").getPart2Result());
